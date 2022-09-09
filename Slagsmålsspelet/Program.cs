c5ca6a1f580d8df7f2d0b3791c9c759304d4a5d4
Nicole Åhgren <ahgren.nicole@elev.ga.ntig.se>
2022-09-09 10:24:42 +0200

    first commit

---
+++ Slagsmålsspelet/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+string Namn1 = "Bubbelmannen";
+string Namn2 = "Molnmannen";
+
+int BubbelHp = 100;
+int MolnHp = 100;
+
+Random generator = new Random();
+
+while (BubbelHp > 0 && MolnHp > 0) {
+Console.BackgroundColor = ConsoleColor.Green;
+    Console.WriteLine("\n~~ Ny Runda ~~");
+ Console.ResetColor();
+
+    Console.WriteLine(" ");
+    Console.WriteLine($"{Namn1}: {BubbelHp} Hp {Namn2}: {MolnHp} Hp\n");
+
+int BubbelSkada = generator.Next(35);
+MolnHp -= BubbelSkada;
+MolnHp = Math.Max(0, MolnHp);
+
+Console.WriteLine($"{Namn1} blåser sprängbubblor så att {Namn2} förlorar {BubbelSkada} Hp");
+
+int MolnSkada = generator.Next(35);
+BubbelHp -= MolnSkada;
+BubbelHp = Math.Max(0, BubbelHp);
+
+Console.WriteLine($"{Namn2} skapar blixtmoln så att {Namn1} förlorar {MolnSkada} Hp");
+
+Console.WriteLine("Tryck ENTER för att fortsätta");
+Console.ReadKey();
+}
+
+Console.WriteLine(" ");
+
+Console.BackgroundColor = ConsoleColor.Red;
+Console.WriteLine("~~ Fighten är över ~~");
+Console.ResetColor();
+
+Console.WriteLine(" ");
+
+if (MolnHp == 0)
+{
+    Console.WriteLine("Bubbelmannen vann med hjälp av sina sprängbubblor!");
+}
+else if (BubbelHp == 0)
+{
+    Console.WriteLine("Molnmannen vann med hjälp av sina blixtrande moln!");
+}
+else if (BubbelHp == 0 && MolnHp == 0)
+{
+    Console.WriteLine("Båda dog samtidigt. Det blev oavgjort!");
+}
+
+Console.WriteLine("Tryck på ENTER för att avsluta");
+Console.ReadKey();
